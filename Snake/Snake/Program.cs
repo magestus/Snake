b040d09dafe63e48f66415790024c900744d4c1b
magestus <magestus@yandex.ru>
2016-04-21 23:39:15 +0500

    1

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,24 +10,10 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Console.SetCursorPosition(x1, y1);
-            Console.Write(sym1);
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-            Console.SetCursorPosition(x2, y2);
-            Console.Write(sym2);
+            Console.WriteLine(" Hello World ");
             Console.ReadLine();
         }
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
         }
     }
-}
+
