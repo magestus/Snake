2ca92fc006b860237c003150df2ac80707a88d60
magestus <magestus@yandex.ru>
2016-04-22 01:08:05 +0500

    11

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Snake;
 
 namespace snake
 {
@@ -14,26 +15,17 @@ namespace snake
             p1.x = 1;
             p1.y = 3;
             p1.sym = '*';
-            Draw(p1.x, p1.y, p1.sym);
+            p1.Draw();
 
 
+            Point p2 = new Point();
+            p2.x = 10;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
 
-
-
-            /*int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Console.SetCursorPosition(x1, y1);
-            Console.Write(sym1);*/
-
- 
             Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
+            
         }
     }
 }
